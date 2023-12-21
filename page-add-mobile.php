33244f53ac89f72e8dd0c33dd241d925a049547f
OXAYR <130404981+OXAYR@users.noreply.github.com>
2023-12-21 11:22:13 +0500

    fixes some bugs and did some stylling

--- page-add-mobile.php
+++ page-add-mobile.php
@@ -1,24 +1,20 @@
 <?php
 
-$show_name = true;
+
 $show_dimensions = true;
 $show_ram = true;
 $show_rom = true;
 $show_front_camera = true;
 $show_back_camera = true;
-$show_price = true;
-$show_image = true;
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $show_name = isset($_POST['show_name']);
+
     $show_dimensions = isset($_POST['show_dimensions']);
     $show_ram = isset($_POST['show_ram']);
     $show_rom = isset($_POST['show_rom']);
     $show_front_camera = isset($_POST['show_front_camera']);
     $show_back_camera = isset($_POST['show_back_camera']);
-    $show_price = isset($_POST['show_price']);
-    $show_image = isset($_POST['show_image']);
 }
 
 get_header();
@@ -27,25 +23,43 @@ get_header();
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-8">
-            <form method="post" id="featureForm">
-                <label><input type="checkbox" name="show_dimensions" <?php echo $show_dimensions ? 'checked' : ''; ?>> Show Dimensions</label><br>
-                <label><input type="checkbox" name="show_ram" <?php echo $show_ram ? 'checked' : ''; ?>> Show RAM</label><br>
-                <label><input type="checkbox" name="show_rom" <?php echo $show_rom ? 'checked' : ''; ?>> Show ROM</label><br>
-                <label><input type="checkbox" name="show_front_camera" <?php echo $show_front_camera ? 'checked' : ''; ?>> Show Front Camera</label><br>
-                <label><input type="checkbox" name="show_back_camera" <?php echo $show_back_camera ? 'checked' : ''; ?>> Show Back Camera</label><br>
-                <input type="submit" value="Submit">
-            </form>
+        <form method="post" id="featureForm" class="mt-3  d-flex flex-row">
+    <div class="form-check mx-2">
+        <input class="form-check-input" type="checkbox" name="show_dimensions" id="show_dimensions" <?php echo $show_dimensions ? 'checked' : ''; ?>>
+        <label class="form-check-label" for="show_dimensions"> Dimensions</label>
+    </div>
+
+    <div class="form-check mx-2">
+        <input class="form-check-input" type="checkbox" name="show_ram" id="show_ram" <?php echo $show_ram ? 'checked' : ''; ?>>
+        <label class="form-check-label" for="show_ram"> RAM</label>
+    </div>
+
+    <div class="form-check mx-2">
+        <input class="form-check-input" type="checkbox" name="show_rom" id="show_rom" <?php echo $show_rom ? 'checked' : ''; ?>>
+        <label class="form-check-label" for="show_rom">ROM</label>
+    </div>
+
+    <div class="form-check mx-2">
+        <input class="form-check-input" type="checkbox" name="show_front_camera" id="show_front_camera" <?php echo $show_front_camera ? 'checked' : ''; ?>>
+        <label class="form-check-label" for="show_front_camera"> Front Camera</label>
+    </div>
+
+    <div class="form-check mx-2">
+        <input class="form-check-input" type="checkbox" name="show_back_camera" id="show_back_camera" <?php echo $show_back_camera ? 'checked' : ''; ?>>
+        <label class="form-check-label" for="show_back_camera"> Back Camera</label>
+    </div>
+
+    <button type="submit" class=" btn rounded-end-pill btn-light">+</button>
+</form>
+
             <?php
             echo do_shortcode(
                 "[mobile_form 
-                    show_name={$show_name} 
                     show_dimensions={$show_dimensions} 
                     show_ram={$show_ram} 
                     show_rom={$show_rom} 
                     show_front_camera={$show_front_camera} 
                     show_back_camera={$show_back_camera} 
-                    show_price={$show_price} 
-                    show_image={$show_image}
                 ]"
             );
             ?>
